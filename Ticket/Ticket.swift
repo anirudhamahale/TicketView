f0ac5064f0ccbb84d1b5f38bf0b3ca44c9037dea
Anirudha <anirudhamahale@gmail.com>
2016-12-23 16:15:51 +0530

    Done.

--- Ticket/Ticket.swift
+++ Ticket/Ticket.swift
@@ -7,15 +7,35 @@
 //
 
 import UIKit
+let π: CGFloat = CGFloat(M_PI)
 
-class Ticket: UIView {
+@IBDesignable class Ticket: UIView {
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
     override func drawRect(rect: CGRect) {
         // Drawing code
+        let context = UIGraphicsGetCurrentContext()
+        let width = self.bounds.width
+        let height = self.bounds.height
+        let controlPointDistance: CGFloat = 3.4
+        let boundingBoxSide: CGFloat = 10
+        
+        CGContextMoveToPoint(context, boundingBoxSide, 0)
+        CGContextAddLineToPoint(context, self.bounds.width - boundingBoxSide, 0)
+//        CGContextAddArc(context, self.bounds.width - 5, 5, 5, 180, 270, 0)
+        CGContextAddCurveToPoint(context, self.bounds.width - boundingBoxSide, controlPointDistance, width - controlPointDistance, boundingBoxSide, self.bounds.width, boundingBoxSide)
+        CGContextAddLineToPoint(context, self.bounds.width, self.bounds.height - boundingBoxSide)
+        CGContextAddCurveToPoint(context, width - controlPointDistance, height - boundingBoxSide, width - boundingBoxSide, height - controlPointDistance, self.bounds.width - boundingBoxSide, self.bounds.height)
+        CGContextAddLineToPoint(context, boundingBoxSide, self.bounds.height)
+        CGContextAddCurveToPoint(context, boundingBoxSide, height - controlPointDistance, controlPointDistance, height - boundingBoxSide, 0, self.bounds.height - boundingBoxSide)
+        CGContextAddLineToPoint(context, 0, boundingBoxSide)
+        CGContextAddCurveToPoint(context, controlPointDistance, boundingBoxSide, boundingBoxSide, controlPointDistance, boundingBoxSide, 0)
+        CGContextSetRGBFillColor(context, 0, 153, 102, 51)
+        CGContextFillPath(context)
+//        CGContextSetLineWidth(context, 2.0)
+//        CGContextSetStrokeColorWithColor(context, UIColor.yellowColor().CGColor)
+        
+//        CGContextSetFillColorWithColor(context, UIColor.blueColor().CGColor)
+        
     }
-    */
-
+    
 }
