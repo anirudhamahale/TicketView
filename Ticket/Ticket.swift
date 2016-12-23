83dbbf0f45b88cde8b04bdcceeed82b998b44f05
Anirudha <anirudhamahale@gmail.com>
2016-12-23 11:43:22 +0530

    first commit

---
+++ Ticket/Ticket.swift
@@ -0,0 +1,21 @@
+//
+//  Ticket.swift
+//  Ticket
+//
+//  Created by LA Argon on 12/23/16.
+//  Copyright © 2016 com.letsappit. All rights reserved.
+//
+
+import UIKit
+
+class Ticket: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
