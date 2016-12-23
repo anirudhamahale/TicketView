354952f2c1d87efd7829c26752a34dced0fd9d64
Anirudha <anirudhamahale@gmail.com>
2016-12-23 10:17:42 +0530

    Initial Commit

---
+++ Ticket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ticket
+//
+//  Created by LA Argon on 12/23/16.
+//  Copyright © 2016 com.letsappit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
